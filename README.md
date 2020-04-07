05c2e5ed1843d97f309775c41cf9cea0a177b8ec
Sidnei Bernardo <sidaobjunior@gmail.com>
2020-04-07 00:11:13 -0300

    About dialog

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # microphone-indicator
 Microphone AppIndicator for Ubuntu 
+
+# Credits 
+Icon made by [Smashicons](https://www.flaticon.com/authors/smashicons) from www.flaticon.com
--- micindicator.py
+++ micindicator.py
@@ -18,11 +18,12 @@ gi.require_version('AppIndicator3', '0.1')
 gi.require_version('Notify', '0.7')
 gi.require_version('Keybinder', '3.0')
 
-from gi.repository import Gtk as gtk
+from gi.repository import Gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify
 from gi.repository import Keybinder
 from threading import Timer
+from gi.repository import GdkPixbuf
 
 
 APPINDICATOR_ID = 'micmuteindicator'
@@ -43,18 +44,38 @@ class Indicator():
         Keybinder.bind(keystr, self.callback_toggle_mic, "keystring %s (user data)" % keystr)
         print ("Press '" + keystr + "' to toggle microphone mute")
 
-    
+    # callback function for the about_action's "activate" signal
+    def show_about_dialog(self, _):
+        aboutdialog = Gtk.AboutDialog()
+
+        authors = ["Sidnei Bernardo Junior"]
+        documenters = ["Sidnei Bernardo Junior"]
+
+        aboutdialog.set_program_name("Microphone AppIndicator for Ubuntu")
+        aboutdialog.set_comments("AppIndicator to mute and show microphone mute status.")
+        aboutdialog.set_logo(GdkPixbuf.Pixbuf.new_from_file(self.get_resource("icon.svg")))
+        aboutdialog.set_authors(authors)
+        aboutdialog.set_documenters(documenters)
+        aboutdialog.set_website("https://github.com/sidneibjunior/microphone-indicator")
+        aboutdialog.set_website_label("Source code at GitHub")
+
+        aboutdialog.show()
 
 
     def callback_toggle_mic(self, keystr, user_data):
         self.toggle_mic(None)
+
+
+    def get_resource(self, resource_name):
+        return os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources'), resource_name)
+    
 
     def get_current_state_icon(self):
         if self.get_current_mic_state() == "[off]":
             icon_name = 'mute.svg'
         else:
             icon_name = 'on.svg'
-        return os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources'), icon_name)
+        return self.get_resource(icon_name)
 
     def get_current_mic_state(self):
         ps = subprocess.Popen(("amixer", "get", "Capture"), stdout=subprocess.PIPE)
@@ -64,13 +85,17 @@ class Indicator():
 
 
     def build_menu(self):
-        menu = gtk.Menu()
+        menu = Gtk.Menu()
 
-        self.item_toggle = gtk.MenuItem('Toggle Microphone')
+        self.item_toggle = Gtk.MenuItem('Toggle Microphone')
         self.item_toggle.connect('activate', self.toggle_mic)
         menu.append(self.item_toggle)
 
-        item_quit1 = gtk.MenuItem('Quit')
+        self.item_about = Gtk.MenuItem('About')
+        self.item_about.connect('activate', self.show_about_dialog)
+        menu.append(self.item_about)
+
+        item_quit1 = Gtk.MenuItem('Quit')
         item_quit1.connect('activate', self.quit1)
         menu.append(item_quit1)
 
@@ -113,12 +138,12 @@ class Indicator():
 
     def quit1(self, _):
         Notify.uninit()
-        gtk.main_quit()
+        Gtk.main_quit()
 
 if __name__ == "__main__":
     Indicator()
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    gtk.main()
+    Gtk.main()
 
 
 
